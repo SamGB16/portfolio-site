d4a5b40b640f4e253d560de72826ec9ba6275e8b
sgb <sgb@njit.edu>
2026-05-08 15:55:20 -0400

    fix asset prefix for github pages

--- samuel-portfolio/next.config.ts
+++ samuel-portfolio/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/portfolio-site',
+  assetPrefix: '/portfolio-site',
   images: {
     unoptimized: true,
   },
